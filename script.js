b1cca629da390604230e2ce59be04bf44b0e6ad3
northlandco <97762980+northlandco@users.noreply.github.com>
2022-04-18 20:01:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -149,6 +149,24 @@ $.getJSON("src/NHLarenas.geojson", function (data){
   controlLayers.addOverlay(geoJsonLayer, 'NHL Arenas');
 });
 
+// First Nations (OpenCanada) (Python Computed --> Shp to Geojson)
+$.getJSON("src/NHLarenas.geojson", function (data){
+  var iconStyle = L.icon({
+    iconUrl: "src/firstnationlogo.png",
+    iconRetinaUrl: 'src/firstnationlogo.png',
+    iconSize: [5, 5]
+  });
+  var geoJsonLayer = L.geoJson(data, {
+    pointToLayer: function( feature, latlng) {
+      var marker = L.marker(latlng,{icon: iconStyle});
+      marker.bindPopup(feature.properties.coordinates); 
+      return marker;
+    }
+  }); // insert ".addTo(map)" to display layer by default
+  controlLayers.addOverlay(geoJsonLayer, 'First Nations');
+});
+
+
 
 
 
